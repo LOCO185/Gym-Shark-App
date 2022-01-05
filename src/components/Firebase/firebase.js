c94a1b425781e512898c4cecf38bc535c8a46374
LOCO185 <jloco151@gmail.com>
2022-01-06 00:37:49 +0200

    netlify bug

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -9,17 +9,7 @@ const firebaseConfig = {
   messagingSenderId: "356564603771",
   appId: "1:356564603771:web:58f96d7ad5eb184f86a3de",
   measurementId: "G-5D8M156YK8",
-  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  // appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  // measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
-
-// console.log(firebaseConfig);
-// console.log(process.env.REACT_APP_FIREBASE_API_KEY);
 
 const app = initializeApp(firebaseConfig);
 
